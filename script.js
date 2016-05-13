b87c663c671624eac2a08c80dbb0c28452e4425a
Ryan <Ryan.fitzpatrick9@gmail.com>
2016-05-12 19:48:06 -0600

    Update script.js

--- script.js
+++ script.js
@@ -13,7 +13,7 @@ module.exports = new Script({
 
     start: {
         receive: (bot) => {
-            return bot.say('So you want to learn about Ryan? Just say HELLO to get started.')
+            return bot.say('So you want to learn about Ryan? Just say HELLO to get started. If you need help, just type "help"')
                 .then(() => 'speak');
         }
     },
